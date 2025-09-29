22f5e279e426cdb169405fdf3007abfa9b15bff2
Nicholas Callahan <ncallahan52@users.noreply.github.com>
2025-09-29 21:36:01 +0000

    final change

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,6 +1,7 @@
 from presidio_anonymizer.sample import sample_run_anonymizer
 
-def test_sample_run_anonymizer_bond_example():
+
+def test_sample_run_anonymizer_bond():
     result = sample_run_anonymizer("My name is Bond.", 11, 15)
 
     assert result.text == "My name is BIP."
