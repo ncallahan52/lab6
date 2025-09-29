20339ee88d6e154fcff812d2c0564a3ece00c5b2
Nicholas Callahan <ncallahan52@users.noreply.github.com>
2025-09-29 21:14:43 +0000

    fix sample_run_anonymizer

--- presidio-anonymizer/presidio_anonymizer/sample.py
+++ presidio-anonymizer/presidio_anonymizer/sample.py
@@ -1,15 +1,19 @@
 from presidio_anonymizer import AnonymizerEngine
 from presidio_anonymizer.entities import RecognizerResult, OperatorConfig
 
-def sample_run_anonymizer(text: str, start: int, end: int, replacement: str = "BIP"):
+
+def sample_run_anonymizer(text: str, start: int, end: int):
     engine = AnonymizerEngine()
     result = engine.anonymize(
         text=text,
-        analyzer_results=[RecognizerResult(entity_type="PERSON", start=start, end=end, score=0.8)],
-        operators={"PERSON": OperatorConfig("replace", {"new_value": replacement})},
+        analyzer_results=[
+            RecognizerResult(entity_type="PERSON", start=start, end=end, score=0.8)
+        ],
+        operators={"PERSON": OperatorConfig("replace", {"new_value": "BIP"})},
     )
     return result
 
+
 if __name__ == "__main__":
-    out = sample_run_anonymizer("My name is Bond.", 11, 15, "BIP")
+    out = sample_run_anonymizer("My name is Bond.", 11, 15)
     print(out)
